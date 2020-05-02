27f9afd3dd5ec5d9d060c15d8fbd134ef28a4dfb
Samantha <samanthabarrueta@gmail.com>
2020-05-02 18:53:05 -0500

    questions set up

---
+++ game.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    
+    var quizLayout = [
+        {
+            question: 'Which of the following is NOT a fundamental programming language?',
+            options: ['javaScript', 'functions', 'CSS'],
+            answer: 'functions',
+        },
+        {
+            question: 'Who can see a console.log?',
+            options: ['the user', 'the developer', 'the dog'],
+            answer: 'the developer',
+        },
+        {
+            question: 'Which of the following is NOT a comparison operator?',
+            options: ['===', '<=', '++'],
+            answer: '++',
+        },
+        {
+            question: 'Which types of collections can be added to an array?',
+            options: ['strings and numbers', 'objects and booleans', 'all of the above'],
+            answer: 'all of the above',
+        },
+        {
+            question: 'Each element in an array is identified by it\'s _______?',
+            options: ['loop', 'index', 'keyword'],
+            answer: 'index',
+        },
+        {
+            question: 'Which of the following can be used to check conditions?',
+            options: ['if/else statements', 'functions', 'loops'],
+            answer: 'if/else statements',
+        },
+        {
+            question: 'Where do we use parameters and arguments?',
+            options: ['in loops', 'in objects', 'in functions'],
+            answer: 'in functions',
+        },
+        {
+            question: 'Which of the following does NOT create a pop up on the screen for the user?',
+            options: ['prompt()', 'console.log()', 'confirm()'],
+            answer: 'console.log()',
+        },
+        {
+            question: 'At what index does an array begin?',
+            options: ['0', '5', '1'],
+            answer: '0',
+        },
+        {
+            question: 'Which of the following is NOT a primitive data type?',
+            options: ['string', 'loop', 'boolean'],
+            answer: 'loop',
+        },
+    ];
+});
